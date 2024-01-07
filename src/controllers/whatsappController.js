4f04f4da38229c7e6e7195111ec3b88b3479672b
JordyV23 <jordyvargas0123@gmail.com>
2024-01-07 11:18:03 -0600

    Configuracion de Webhook
    -Configuracion de webhook en el portal de meta for developers
    -Implementacion de funciones para verificar el token - recibir mensajes desde el chat de whatsapp

--- src/controllers/whatsappController.js
+++ src/controllers/whatsappController.js
@@ -1,13 +1,40 @@
+const fs = require("fs");
+const myConsole = new console.Console(fs.createWriteStream("./log.txt",{ flags: 'a' }))
 
-const verifyToken = (req,res) => {
-    res.send("Hola");
-}
+const verifyToken = (req, res) => {
+  try {
+    let accessToken = "jxe60pmHqdYmcClzX58wQji";
+    let token = req.query["hub.verify_token"];
+    var challenge = req.query["hub.challenge"];
 
-const receiveMessage = (req,res) => {
-    res.send("Hola");
-}
+    if (challenge != null && token != null && token == accessToken) {
+      res.send(challenge);
+    } else {
+      res.status(400).send();
+    }
+  } catch (error) {
+    res.status(400).send();
+  }
+};
+
+const receiveMessage = (req, res) => {
+  try {
+
+    var entry = (req.body['entry'])[0]
+    var changes = (entry['changes'])[0]
+    var value = changes["value"]
+    var messageObject = value['messages']
+
+    myConsole.log(messageObject)
+
+    res.send("EVENT_RECEIVED");
+  } catch (error) {
+    myConsole.log(error)
+    res.send("EVENT_RECEIVED");
+  }
+};
 
 module.exports = {
-    verifyToken,
-    receiveMessage
-}
+  verifyToken,
+  receiveMessage,
+};
